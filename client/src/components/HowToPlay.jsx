2cd16cb2a1c4657351f9a72a8e04f26521b3b8d6
Minh Le <phle0409@gmail.com>
2022-02-21 07:19:32 -0800

    Done How To Play Page

--- client/src/components/HowToPlay.jsx
+++ client/src/components/HowToPlay.jsx
@@ -195,14 +195,184 @@ export default function HowToPlay() {
                 <img
                   className="img-fluid"
                   src={require('../images/how_to_play/type_3.png')}
-                  alt="prize_card"
+                  alt="type_3"
                 />
               </div>
             </div>
           </section>
         </Tab>
 
-        <Tab eventKey="play" title="How To Play"></Tab>
+        <Tab eventKey="play" title="How To Play">
+          <h1 class="text-secondary fw-bold text-center">How To Play</h1>
+
+          <section className="bg-white my-4 ">
+            <div className="row">
+              <div className="col-6">
+                <h3 className="pt-3 text-info fw-bold ps-2">
+                  {' '}
+                  Setting Up To Play
+                </h3>
+                <ol className="ps-5">
+                  <li className="lead">Shake hands with your opponent.</li>
+                  <li className="lead">
+                    Flip a coin. The winner of the coin flip decides which
+                    player goes first.
+                  </li>
+                  <li className="lead">
+                    Shuffle your 60-card deck and draw the top 7 cards.
+                  </li>
+                  <li className="lead">
+                    Check to see if you have any Basic Pokémon in your hand.
+                  </li>
+                  <li className="lead">
+                    Put one of your Basic Pokémon face down as your Active
+                    Pokémon.
+                  </li>
+                  <li className="lead">
+                    Put up to 5 more Basic Pokémon face down on your Bench.
+                  </li>
+                  <li className="lead">
+                    Put the top 6 cards of your deck off to the side face down
+                    as your Prize cards.
+                  </li>
+                  <li className="lead">
+                    Both players flip their Active and Benched Pokémon face up
+                    and start the game!
+                  </li>
+                </ol>
+              </div>
+
+              <div className="col-6">
+                <img
+                  className="img-fluid"
+                  src={require('../images/how_to_play/setting_up.png')}
+                  alt="setting_up"
+                />
+              </div>
+            </div>
+          </section>
+
+          <section className="bg-light my-4 ">
+            <div>
+              <h3 className="pt-3 text-info fw-bold ps-2"> Parts of a Turn</h3>
+              <ol className="ps-5">
+                <li className="fw-bold lead">Draw a card.</li>
+                <li className="fw-bold lead">
+                  Do any of the following actions in any order:
+                  <ol type="A" className="fw-light">
+                    <li>
+                      Put Basic Pokémon cards from your hand onto your Bench (as
+                      many as you want).
+                    </li>
+                    <li>Evolve your Pokémon (as many as you want).</li>
+                    <li>
+                      Attach an Energy card from your hand to one of your
+                      Pokémon (once per turn).
+                    </li>
+                    <li>
+                      Play Trainer cards (as many as you want, but only one
+                      Supporter card and one Stadium card per turn).
+                    </li>
+                    <li>Retreat your Active Pokémon (only once per turn).</li>
+                    <li>Use Abilities (as many as you want).</li>
+                  </ol>
+                </li>
+                <li className="fw-bold lead">
+                  Attack. Then, end your turn.
+                  <ol type="A" className="fw-light">
+                    <div>
+                      <li className="fw-bold">
+                        CHECK the Energy attached to your Active Pokémon.
+                      </li>
+                      <div className="row">
+                        <div className="col-6">
+                          <p>
+                            You need the right amount of Energy attached to a
+                            Pokémon for it to attack. For example, look at
+                            Drizzile. Its Rain Splash attack costs 1 water
+                            enery, so you must have at least 1 water Energy
+                            attached to Drizzile to use this attack.
+                          </p>
+                        </div>
+                        <div className="col-6">
+                          <img
+                            className="img-fluid"
+                            src={require('../images/how_to_play/attack.png')}
+                            alt="attack"
+                          />
+                        </div>
+                      </div>
+                    </div>
+                    <div>
+                      <li className="fw-bold">
+                        CHECK Weakness and Resistance of your opponent’s Active
+                        Pokémon.
+                      </li>
+                      <p>
+                        Some Pokémon have Weakness or Resistance to Pokémon of
+                        certain types, marked in the lower-left corner of the
+                        card. If the attack does damage, your opponent’s Active
+                        Pokémon takes more damage if it has Weakness to the
+                        attacker’s type. It takes less damage from a Pokémon if
+                        it has Resistance to that Pokémon’s type.
+                      </p>
+                    </div>
+
+                    <div>
+                      <li className="fw-bold">
+                        PUT damage counters on your opponent’s Active Pokémon.
+                      </li>
+                      <p>
+                        When you attack, put 1 damage counter on your opponent’s
+                        Active Pokémon for each 10 damage your Pokémon’s attack
+                        does (written to the right of the attack name). In the
+                        example to the right, Scorbunny’s Tackle attack does 10
+                        damage. Then, Grookey’s Weakness of ×2 to Pokémon makes
+                        that 10 ×2 = 20 damage. So put 2 damage counters on
+                        Grookey. If an attack says to do something else, be sure
+                        to do that, too! Your attack is complete, so check to
+                        see if any Pokémon were Knocked Out by the attack. Some
+                        attacks can damage more than one Pokémon, and sometimes
+                        they can even damage the Attacking Pokémon! So, make
+                        sure to check every Pokémon that was affected by the
+                        attack. If a Pokémon has total damage at least equal to
+                        its HP (for example, 5 or more damage counters on a
+                        Pokémon with 50 HP), it is Knocked Out. If a player’s
+                        Pokémon is Knocked Out, that player puts it and all
+                        cards attached to it in the discard pile. That player’s
+                        opponent takes 1 of their own Prize cards and puts it
+                        into their hand. The player whose Pokémon was Knocked
+                        Out chooses a new Active Pokémon from their Bench. If
+                        your opponent can’t do this because their Bench is empty
+                        (or for any other reason), you win the game! If your
+                        opponent still has Pokémon in play, but you just took
+                        your last Prize card, you also win the game!
+                      </p>
+                    </div>
+
+                    <div>
+                      <li className="fw-bold">Your turn is over.</li>
+                      <p>
+                        Next, you take care of a few special things during
+                        Pokémon Checkup.
+                      </p>
+                    </div>
+                  </ol>
+                </li>
+              </ol>
+            </div>
+          </section>
+
+          <section>
+            <h3 className="fw-bold text-info ps-2">More Rules & Info</h3>
+            <div className="ps-4">
+              <p>For more rules and information, you can check this link:</p>
+              <a href="https://www.pokemon.com/us/pokemon-tcg/">
+                https://www.pokemon.com/us/pokemon-tcg/
+              </a>
+            </div>
+          </section>
+        </Tab>
       </Tabs>
       <div className="text-center mt-4">
         <a href="/" className="btn btn-primary btn-lg">
